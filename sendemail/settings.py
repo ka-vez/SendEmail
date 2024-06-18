73a347f48509b96d6fe851abbbe75ba0de2740e4
Adegbite David <168303668+ka-vez@users.noreply.github.com>
2024-06-18 22:18:53 +0100

    Update settings.py

--- sendemail/settings.py
+++ sendemail/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['.vercel.app']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kavez.django.login@gmail.com'
-EMAIL_HOST_PASSWORD = 'reba cdbh niwa fmlv'
+EMAIL_HOST_PASSWORD = 'jeho czdy lwzm auzl'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
